7f72ae4d5f7d14c74cc0378cf856859d225584a8
UX014563 <UX014563@TP6VDIDEVTCM222.ten.thomsonreuters.com>
2019-06-17 17:56:38 +0530

    commit

--- VCSTAX_REWRITE/src/test/java/com/vcstax/testCases/TC_Smoke_ActivityTab_02.java
+++ VCSTAX_REWRITE/src/test/java/com/vcstax/testCases/TC_Smoke_ActivityTab_02.java
@@ -17,7 +17,6 @@ public class TC_Smoke_ActivityTab_02 extends BaseClass {
 		loginPage = new LoginPage(driver);
 		activityTab = new ActivityTab(driver);
 		driver.get(vcstaxURL);
-		System.out.println();
 	}
 	
 	@Test(priority = 0)
